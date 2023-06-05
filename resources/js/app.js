aa7aa66ec51a3ba00e72f7b690f1311210086571
Devinson Alvarez <dalvarez@sistemasmitdev.com>
2023-06-05 14:12:14 -0500

    Installed dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import "./bootstrap";
+
+import Alpine from "alpinejs";
+import mask from "@alpinejs/mask";
+import persist from "@alpinejs/persist";
+import collapse from "@alpinejs/collapse";
+
+window.Alpine = Alpine;
+
+Alpine.plugin(mask);
+Alpine.plugin(persist);
+Alpine.plugin(collapse);
+
+Alpine.start();
---
+++ resources/views/test.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ad suscipit atque quos a totam eveniet soluta recusandae tempore optio, blanditiis esse vero facere incidunt doloribus! Aspernatur culpa doloribus cum.
+</x-app-layout>
