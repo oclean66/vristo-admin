a456e6a11355cb2d1542ee72d98e92992fde489f
Jean Paul <jduarte@sistemasmitdev.com>
2023-07-01 11:42:06 -0500

    CU-863h3gtvp - Deshabilitar registro de usuarios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <x-blank-layout>
     @if (Route::has('login'))
-        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10 flex items-center">
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10 flex items-center mx-3">
             <div class="mr-4">
                 <x-interface.lang-switcher />
             </div>
@@ -17,10 +17,10 @@
                 <a href="{{ route('login') }}"
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">{{ __('Login') }}</a>
 
-                @if (Route::has('register'))
+                {{-- @if (Route::has('register'))
                     <a href="{{ route('register') }}"
                         class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 ml-4">{{ __('Register') }}</a>
-                @endif
+                @endif --}}
             @endauth
         </div>
     @endif
